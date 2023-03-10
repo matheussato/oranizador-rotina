494c0e0c3d39aae4cba6fbc031f7678d895a28de
matheussato <dssato03@gmail.com>
2023-03-10 10:28:47 -0300

    feat: Adicionando aquivos do Spring Initializr

---
+++ src/main/java/br/com/fiap/organizadortarefas/OrganizadorTarefasApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.organizadortarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizadorTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizadorTarefasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/organizadortarefas/OrganizadorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.organizadortarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizadorTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
